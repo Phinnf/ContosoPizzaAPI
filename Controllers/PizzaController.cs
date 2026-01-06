df65d982a0bab50910f504ed677f447266b6bdab
Phinnf <bnm915915@gmail.com>
2026-01-06 16:03:32 +0700

    Continue testing

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -1,11 +1,34 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ContosoPizza.Models;
+using ContosoPizza.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ContosoPizza.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class PizzaController : ControllerBase
     {
+        public PizzaController()
+        {
+        }
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAllPizzas() => PizzaService.GetAll();
+
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id)
+        {
+            var pizza = PizzaService.Get(id);
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+            return pizza;
+        }
+        [HttpPost]
+        public ActionResult Create(Pizza pizza)
+        {
+            PizzaService.Add(pizza);
+            return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
+        }
     }
 }
